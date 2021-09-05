78bbab2b992ac2eb46f145b44705faf117f94dda
ZhaoHouLin <rodes5292@gmail.com>
2021-09-05 14:36:38 +0800

    to finish ch10

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,330 @@
 
+<script>
+import { ref } from '@vue/reactivity'
+import { onMounted } from 'vue'
+
+export default {
+  setup() {
+
+
+    const plusBtn = ref()
+    const msgBtn = ref()
+    const notificationBtn = ref()
+    const moreBtn = ref()
+
+    const plusPanel = ref()
+    const msgPanel = ref()
+    const notificationPanel = ref()
+    const morePanel = ref()
+
+    const leftBlock = ref()
+    const rightBlock = ref()
+
+    const renderLeftItem = (name,imgUrl)=> {
+      const leftItem =`
+        <div
+          class="
+            flex
+            items-center
+            justify-items-center
+            w-full
+            p-2
+            mb-1
+            rounded
+            hover:bg-fb-input
+            cursor-pointer
+          "
+        >
+          <div class="w-[32px] overflow-hidden rounded-full mr-4">
+            <img src="${imgUrl}" alt="" />
+          </div>
+          <p class="text-white text-[.9rem]">${name}</p>
+        </div>
+      `
+      return leftItem
+    }
+
+    const renderLeftBlock = ()=> {
+
+      const leftArr = [
+        {
+          name: "Raiden",
+          img: "https://bruce-fe-fb.web.app/image/avator.png",
+        },
+        {
+          name: "活動",
+          img: "https://bruce-fe-fb.web.app/image/left/activity.svg",
+        },
+        {
+          name: "天氣",
+          img: "https://bruce-fe-fb.web.app/image/left/cloudy.png",
+        },
+        {
+          name: "災害應變中心",
+          img: "https://bruce-fe-fb.web.app/image/left/dynamic.svg",
+        },
+        {
+          name: "新冠病毒資訊中心",
+          img: "https://bruce-fe-fb.web.app/image/left/facemask.svg",
+        },
+        {
+          name: "社團",
+          img: "https://bruce-fe-fb.web.app/image/left/friend.svg",
+        },
+        {
+          name: "企業管理平台",
+          img: "https://bruce-fe-fb.web.app/image/left/job.png",
+        },
+        {
+          name: "Messenger",
+          img: "https://bruce-fe-fb.web.app/image/left/messenger.svg",
+        },
+        {
+          name: "近期廣告動態",
+          img: "https://bruce-fe-fb.web.app/image/left/pay.png",
+        },
+        {
+          name: "朋友名單",
+          img: "https://bruce-fe-fb.web.app/image/left/sale.png",
+        },
+        {
+          name: "最愛",
+          img: "https://bruce-fe-fb.web.app/image/left/star.svg",
+        },
+        {
+          name: "Marketplace",
+          img: "https://bruce-fe-fb.web.app/image/left/store.svg",
+        },
+        {
+          name: "Watch",
+          img: "https://bruce-fe-fb.web.app/image/left/watchingTv.svg",
+        },
+      ];
+
+      let htmlStr= ''
+
+      leftArr.forEach(element => {
+        htmlStr += renderLeftItem(element.name,element.img)
+      });
+
+      leftBlock.value.innerHTML = htmlStr
+    }
+
+    const renderRightItem = (name,imgUrl)=> {
+      const rightItem = `
+        <div
+          class="
+            flex
+            items-center
+            justify-items-center
+            w-full
+            py-2
+            px-1
+            rounded
+            hover:bg-fb-input
+            cursor-pointer
+          "
+        >
+          <div class="w-[45px]">
+            <div class="relative w-[32px] cursor-pointer">
+              <div class="overflow-hidden rounded-full">
+                <img src=${imgUrl} alt="" />
+              </div>
+              <div
+                class="
+                  w-[8px]
+                  h-[8px]
+                  rounded-full
+                  bg-green-500
+                  absolute
+                  bottom-0
+                  right-0
+                  ring-gray-900 ring
+                "
+              ></div>
+            </div>
+          </div>
+          <p class="text-white text-[.9rem]">${name}</p>
+        </div>
+      `
+      return rightItem
+    }
+
+    const renderRightBlock = ()=> {
+
+      const rightArr = [
+        {
+          name: "Raiden",
+          img: "https://bruce-fe-fb.web.app/image/avator.png",
+        }
+      ];
+
+      let htmlStr= '<p class="mb-2 text-lg text-gray-400">聯絡人</p>'
+
+      for (let index = 0; index < 10; index++) {
+        htmlStr += renderRightItem(rightArr[0].name,rightArr[0].img)
+      }
+      // rightArr.forEach(element => {
+      //   htmlStr += renderRightItem(element.name,element.img)
+      // });
+
+      rightBlock.value.innerHTML = htmlStr
+    }
+
+
+    const panels = ref([plusPanel,msgPanel,notificationPanel,morePanel])
+
+    const openPanel = (index,event)=> {
+      // console.log(index,event);
+      event.stopPropagation()
+      panels.value.forEach((element,idx) => {
+        if(index===idx) {
+          element.value.classList.remove('hidden')
+          return
+        }
+        if(element.value.classList.contains('hidden')) {
+          return
+        }
+        element.value.classList.add('hidden')
+      });
+    }
+
+
+    //取消傳遞window被點
+    const panelStopPropagation= (e)=> {
+      e.stopPropagation();
+    }
+
+    onMounted(() => {
+      window.addEventListener('click',(event)=> {
+        openPanel(-1,event)
+        // console.log('window');
+      })
+
+      renderLeftBlock()
+      renderRightBlock()
+    })
+
+
+    return {
+      plusBtn,
+      plusPanel,
+      msgBtn,
+      msgPanel,
+      notificationBtn,
+      notificationPanel,
+      moreBtn,
+      morePanel,
+      panelStopPropagation,
+      openPanel,
+      // renderLeftItem,
+      leftBlock,
+      rightBlock
+    }
+  }
+}
+</script>
+
 
 <template lang='pug'>
-h1.text-fb hi
+header(class="w-full h-[56px] bg-fb-header fixed top-0 left-0 right-0 flex items-center justify-between px-4")
+
+  //- header left
+  div(class="flex items-center")
+    svg.fill-current.text-fb(viewBox='0 0 36 36' height='39' width='39')
+      defs
+        lineargradient(x1='50%' x2='50%' y1='97.0782153%' y2='0%')
+          stop(offset='0%' stop-color='#0062E0')
+          stop(offset='100%' stop-color='#19AFFF')
+      path(d='M15 35.8C6.5 34.3 0 26.9 0 18 0 8.1 8.1 0 18 0s18 8.1 18 18c0 8.9-6.5 16.3-15 17.8l-1-.8h-4l-1 .8z')
+      path.fill-current.text-white(d='M25 23l.8-5H21v-3.5c0-1.4.5-2.5 2.7-2.5H26V7.4c-1.3-.2-2.7-.4-4-.4-4.1 0-7 2.5-7 7v4h-4.5v5H15v12.7c1 .2 2 .3 3 .3s2-.1 3-.3V23h4z')
+
+
+    //- search bar (只在電腦版顯示)
+    button.popover-btn(class='lg:hidden')
+      img(src='https://bruce-fe-fb.web.app/image/search.svg')
+    .bg-fb-input.ml-2.py-2.px-3.rounded-full.items-center.hidden(class='w-[240px] lg:flex')
+      svg.mr-2(xmlns='http://www.w3.org/2000/svg' class='w-[18px] h-[18px]' fill='none' viewBox='0 0 24 24' stroke='#9ba3af')
+        path(stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z')
+      input.w-full.bg-transparent.text-white(type='text' class='focus:outline-none' placeholder='搜尋 Facebook')
+
+
+  //- header right
+  .flex.items-center.relative
+    button#plus-btn.popover-btn(ref='plusBtn' @click='openPanel(0,$event)')
+      img(src='https://bruce-fe-fb.web.app/image/plus.svg')
+    button#msg-btn.popover-btn(ref='msgBtn' @click='openPanel(1,$event)')
+      img(src='https://bruce-fe-fb.web.app/image/messenger.svg')
+    button#notification-btn.popover-btn(ref='notificationBtn' @click='openPanel(2,$event)')
+      img(src='https://bruce-fe-fb.web.app/image/bell.svg')
+    button#more-btn.popover-btn(ref='moreBtn' @click='openPanel(3,$event)')
+      img(src='https://bruce-fe-fb.web.app/image/down.svg' )
+    
+    //- plus
+    #plus-panel.popover-panel.hidden(ref='plusPanel' @click='panelStopPropagation')
+      p.text-white.mb-3.text-2xl 建立
+      .flex.items-center.p-1.mb-2.rounded-lg.cursor-pointer(class='hover:bg-fb-input')
+        .bg-fb-input.rounded-full.p-2.mr-2.flex.justify-center.items-center
+          svg(xmlns='http://www.w3.org/2000/svg' fill='none' width='22' height='22' viewBox='0 0 24 24' stroke='white')
+            path(stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z')
+        .flex-1
+          p.text-sm.text-white(class='mb-[2px]') 貼文
+          p.text-xs.text-gray-400 在動態消息分享貼文。
+    
+    //- messenger
+    #msg-panel.popover-panel.hidden(ref='msgPanel' @click='panelStopPropagation')
+      p.text-white.mb-3.text-2xl Messsenger
+      .bg-fb-input.rounded-full.py-1.px-3.flex.items-center.mb-4
+        svg.mr-2(xmlns='http://www.w3.org/2000/svg' class='w-[18px] h-[18px]' fill='none' viewBox='0 0 24 24' stroke='#9ba3af')
+          path(stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z')
+        input.w-full.bg-transparent.text-white(type='text' class='focus:outline-none' placeholder='搜尋 Messsenger')
+      .flex.items-center.mb-2.p-2.rounded-lg.cursor-pointer(class='hover:bg-fb-input')
+        .rounded-full.overflow-hidden.mr-3(class='w-[50px] h-[50px]')
+          img.w-full.object-cover(src='https://bruce-fe-fb.web.app/image/avator.png' alt='')
+        .flex-1
+          p.text-base.text-white(class='mb-[2px]') 布魯斯
+          p.text-sm.text-gray-400 TailwindCSS讚讚！・1分鐘前
+    
+    //- notification
+    #notification-panel.popover-panel.hidden(ref='notificationPanel' @click='panelStopPropagation')
+      p.text-white.mb-3.text-2xl 通知
+      .flex.items-center.mb-2.p-2.rounded-lg.cursor-pointer(class='hover:bg-fb-input')
+        .rounded-full.overflow-hidden.mr-3(class='w-[50px] h-[50px]')
+          img.w-full.object-cover(src='https://bruce-fe-fb.web.app/image/avator.png' alt='')
+        .flex-1
+          p.text-sm.text-white(class='mb-[2px]')
+            | 布魯斯發佈了一門新課程！
+          p.text-xs.text-fb 1分鐘前
+    //- more      
+    #more-panel.popover-panel.hidden(ref='morePanel' @click='panelStopPropagation')
+      .flex.items-center.mb-2.p-2.rounded-lg.cursor-pointer(class='hover:bg-fb-input')
+        .rounded-full.overflow-hidden.mr-3(class='w-[50px] h-[50px]')
+          img.w-full.object-cover(src='https://bruce-fe-fb.web.app/image/avator.png' alt='')
+        .flex-1
+          p.text-base.text-white(class='mb-[2px]') 林炤后
+          p.text-sm.text-gray-400 查看你的個人檔案
+      .flex.p-1.mb-2.items-center.rounded-lg.cursor-pointer(class='hover:bg-fb-input')
+        .bg-fb-input.rounded-full.p-2.mr-2.flex.justify-center.items-center
+          svg(xmlns='http://www.w3.org/2000/svg' fill='none' width='22' height='22' viewBox='0 0 24 24' stroke='white')
+            path(stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1')
+        p.flex-1.text-white 登出
+  
+      
+
+main(class="min-h-[100vh] pt-main-span flex")
+
+  //- div(class="hidden sticky top-main-span max-w-[360px] w-full h-full lg:block")
+  //-   p(class="w-full bg-blue-300") 左側資訊欄
+  #left-block.hidden.sticky.top-main-span.w-full.h-full.pl-4(class='max-w-[360px] lg:block' ref='leftBlock')
+
+  div(class="max-w-[744px] w-full mx-auto h-[200vh]")
+    p(class="w-full bg-white") 動態牆
+
+  //- div(class="hidden sticky top-main-span max-w-[360px] w-full h-full lg:block")
+  //-   p(class="w-full bg-green-300") 右側聯絡人
+  #right-block.hidden.sticky.top-main-span.w-full.h-full(class='max-w-[360px] lg:block' ref='rightBlock')
+  //- #right-block.hidden.sticky.top-main-span.w-full.h-full(class='max-w-[360px] lg:block' ref='rightBlock' style='z-index: -1')
+
 </template>
 
 <style lang='stylus'>
@@ -9,9 +332,16 @@ h1.text-fb hi
   font-family Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
+  // text-align center
   color #2c3e50
   // margin-top 60px
   height 100vh
 
+#right-block
+  z-index 0
+
+// .popover-panel
+//   position fixed
+//   z-index 999
+//   margin-right  3rem
 </style>
